4f47fd7dd7fd4c455adad0d12bb53923226f249e
Ashley <ashley.teo@ufinity.com>
2022-12-01 01:04:44 +0800

    [Ashley] add prettier and eslint

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 const Div = styled.div`
-background-color: red;
-font-size: 5rem`;
+	background-color: red;
+	font-size: 5rem;
+`;
 
-export const Button = () => <Div>hello world</Div>
+export const Button = () => <Div>hello world</Div>;
