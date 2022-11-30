eca7a368148391cf2dd71e2f1466489777a206f3
Ashley <ashley.teo@ufinity.com>
2022-12-01 00:26:11 +0800

    [Ashley] setup vite config and sample component

---
+++ src/stories/Button.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+background-color: red;
+font-size: 5rem`;
+
+export const Button = () => <Div>hello world</Div>
---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+    plugins: [
+        react({
+          jsxRuntime: "classic"
+        }),
+        dts({
+            insertTypesEntry: true,
+        }),
+    ],
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, 'src/index.ts'),
+            name: 'dist',
+            formats: ['es', 'cjs'],
+            fileName: (format) => `index.${format}.js`,
+        },
+        rollupOptions: {
+            external: ['react', 'react-dom', 'styled-components', 'tslib'],
+            output: {
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
+                    'styled-components': 'styled',
+                },
+            },
+        },
+    },
+});
